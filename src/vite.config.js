2d613d4e653394d00dc95ca68dd998288ab2559b
Fernando Vegas <vegasfernando2003@gmail.com>
2023-07-23 11:50:15 -0500

    prodcuts page adde

--- src/vite.config.js
+++ src/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig ({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        login: resolve (__dirname, 'login/index.html')
+        login: resolve (__dirname, 'login/index.html'),
+        products: resolve (__dirname, 'products/index.html')
       }
     }
   }
